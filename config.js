f59c207cc63c0c7023474239035d828eb5587278
ZaggyFatty <mohantysama@gmail.com>
2026-02-05 17:59:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
+            text: "Will you be my Valentine on February 14th, 2026? 🌹🦌💝🦆", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
